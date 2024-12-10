d4a014ff05d9f77a15d5566df354f43249fb40e6
Filip Rad <filip.radziminski@wp.pl>
2024-12-10 22:24:17 +0100

    dunno

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,67 @@
 <template>
-    <main class="w-screen bg-[#EAA45D]">
-        <div class="text-white">
-            <div class="relative">
-                <div class="h-[54rem] w-full">
-                    <img
-                        src="https://media.discordapp.net/attachments/931577235146678332/1316133408665243658/Travel-Wallpaper-Free-Download-948851720.jpg?ex=6759f041&is=67589ec1&hm=91631128b9655f22283c8570265e5665b6c2ce4bcd5e5c10514058f1d4a3445f&=&format=webp&width=1466&height=910"
-                        class="h-full w-full object-cover"
-                        alt=""
-                    />
-                </div>
-                <div class="absolute top-30 right-40 bg-black w-160 p-10 text-left">
-                    <div class="text-9">Zapraszamy do Koła</div>
-                    <div class="text-9">Turystyki i Marketingu!!!</div>
-                    <div class="text-6 pt-4">
-                        Odkrywaj, twórz, rozwijaj się! <br />Kółko naukowe: miejsce, gdzie pasja spotyka naukę.
-                    </div>
+    <main class="w-screen bg-[#EAA45D] text-white">
+        <div class="relative">
+            <div class="h-[54rem] w-full">
+                <img
+                    src="https://images-ext-1.discordapp.net/external/lvidjD-qF345Eda5IeiqkIdrf4-EMBU32OGad3XPyzc/https/www.pixelstalk.net/wp-content/uploads/images6/Travel-Wallpaper-Free-Download.jpg?format=webp&width=1089&height=676"
+                    class="h-full w-full object-cover"
+                    alt=""
+                />
+            </div>
+            <div class="absolute top-30 right-40 bg-[#EAA45D] w-160 p-10 text-black">
+                <div class="text-9">Zapraszamy do Koła <br />Turystyki i Marketingu!!!</div>
+                <div class="text-6 pt-4">Z nami rozwiniesz skrzydła!</div>
+            </div>
+        </div>
+        <div class="grid grid-cols-2 gap-20 py-30 px-70">
+            <div class="h-[40rem] w-full">
+                <img
+                    src="https://sumbcq.webwave.dev/files/dynamicContent/sites/sumbcq/images/pl/webpage_1/lqijiye9/element_19/rwdMode_1/550x600/unsplash-image.webp"
+                    class="h-full w-full object-cover"
+                    alt=""
+                />
+            </div>
+            <div class="text-left">
+                <div class="font-extrabold text-16 pb-8">O nas</div>
+                <div class="text-7 pr-8 leading-16">
+                    Jesteśmy dynamiczną społecznością studencką zafascynowaną ekonomią oraz nowoczesnymi usługami. Nasze
+                    koło skupia się na rozwoju wiedzy poprzez badania, praktykę oraz interaktywne seminaria. Tworzymy
+                    inspirujące projekty badawcze i integrujące wyjazdy, zapewniając platformę do eksploracji
+                    najnowszych trendów w tych dziedzinach.
                 </div>
             </div>
-            <div class="grid grid-cols-2 gap-20 py-30 px-70">
-                <div class="h-[40rem] w-full">
-                    <img
-                        src="https://sumbcq.webwave.dev/files/dynamicContent/sites/sumbcq/images/pl/webpage_1/lqijiye9/element_19/rwdMode_1/550x600/unsplash-image.webp"
-                        class="h-full w-full object-cover"
-                        alt=""
-                    />
-                </div>
-                <div class="text-left">
-                    <div class="font-extrabold text-16 pb-8">O nas</div>
-                    <div class="text-7 pr-8 leading-16">
-                        Jesteśmy dynamiczną społecznością studencką zafascynowaną ekonomią oraz nowoczesnymi usługami.
-                        Nasze koło skupia się na rozwoju wiedzy poprzez badania, praktykę oraz interaktywne seminaria.
-                        Tworzymy inspirujące projekty badawcze i integrujące wyjazdy, zapewniając platformę do
-                        eksploracji najnowszych trendów w tych dziedzinach.
-                    </div>
-                </div>
-            </div>
-            <div class="">
-                <div class="">Nasza misja oraz co możemy zaoferować</div>
-                <div class="grid grid-cols-3 px-7 py-11 text-5">
-                    <div class="grid grid-rows-2">
-                        <div class="flex gap-4">
-                            <div class="w-4 h-full bg-[#173f35]"></div>
-                            <div class="">
-                                Rozwój umiejętności analitycznych w obszarze ekonomii i usług. Tworzenie innowacyjnych
-                                projektów badawczych. Budowanie sieci kontaktów z profesjonalistami z branży
-                            </div>
-                        </div>
-                        <div class="flex">
-                            <div class="w-[2px] h-[1em] bg-[#173f35]"></div>
-                            <div class="">
-                                Interaktywne seminaria prezentujące najnowsze tendencje w dziedzinie ekonomii. Projekty
-                                badawcze zachęcające do eksploracji nowatorskich pomysłów.
-                            </div>
+        </div>
+        <div class="">
+            <div class="text-9 text-center">Nasza misja oraz co możemy zaoferować</div>
+            <div class="grid grid-cols-3 px-7 py-11 text-5">
+                <div class="grid gap-5">
+                    <div class="flex gap-4">
+                        <div class="w-3 h-full bg-[#173f35]"></div>
+                        <div class="">
+                            Rozwój umiejętności analitycznych w obszarze ekonomii i usług. Tworzenie innowacyjnych
+                            projektów badawczych. Budowanie sieci kontaktów z profesjonalistami z branży
                         </div>
                     </div>
-                    <div class=""></div>
-                    <div class="grid grid-rows-2">
+                    <div class="flex gap-4">
+                        <div class="w-3 h-full bg-[#173f35]"></div>
+
+                        <div class="">
+                            Interaktywne seminaria prezentujące najnowsze tendencje w dziedzinie ekonomii. Projekty
+                            badawcze zachęcające do eksploracji nowatorskich pomysłów.
+                        </div>
+                    </div>
+                </div>
+                <div class=""></div>
+                <div class="grid gap-5">
+                    <div class="flex gap-4">
+                        <div class="w-3 h-full bg-[#173f35]"></div>
                         <div class="">
                             Integracyjne wyjazdy krajoznawcze i międzynarodowe, poszerzające horyzonty. Możliwość
                             nawiązania kontaktów z doświadczonymi praktykami z branży.
                         </div>
+                    </div>
+                    <div class="flex gap-4">
+                        <div class="w-3 h-full bg-[#173f35]"></div>
                         <div class="">
                             Dołączając do naszego koła, otrzymasz szansę na rozwijanie wiedzy, zdobywanie cennych
                             doświadczeń oraz współtworzenie innowacyjnych projektów. Zapraszamy Cię do przyłączenia się
