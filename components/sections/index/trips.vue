a8ad92aa365ce1f5ba3603cea6aa306ff2ea06e7
Filip Rad <filip.radziminski@wp.pl>
2024-12-11 21:31:24 +0100

    added trips

---
+++ components/sections/index/trips.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="px-40 py-12">
+        <div class="text-9 text-center font-bold">Nasze wyjazdy</div>
+        <div class="grid grid-cols-2 text-5 pt-12 gap-12 items-start">
+            <div class="grid gap-6">
+                <div class="font-bold italic text-center">II Pomorskie Seminarium Geograficzne</div>
+                <div class="font-bold italic text-center">
+                    pt. ,,Pomorze w postpandemicznej i wojennej rzeczywistości’’
+                </div>
+                <div class="">
+                    W dniu 15 czerwca 2023r. odbyło się II Pomorskie Seminarium Geograficzne pt. ,,Pomorze w
+                    postpandemicznej i wojennej rzeczywistości’’ w Instytucie Geografii Społeczno Ekonomicznej
+                    Uniwersytetu Pomorskiego w Słupsku. W ramach działalności Naukowego koła Ekonomiki i Usług
+                    ,,WannaGo’’ pod czujnym okiem Doktor Marzeny Wanagos został wygłoszony referat zatytułowany ,,Zmiany
+                    preferencji zakwaterowania podczas krajowych wyjazdów Polaków w okresie przed i po pandemii
+                    Covid19’’. Uniwersytet Morski w Gdynii był reprezentowany przez Doktor Marzenę Wanagos oraz jego
+                    studentów jednocześnie członków koła WannaGo : Patrycję Krupę, Paulinę Mikułę oraz Aleksandra Sapko.
+                </div>
+            </div>
+            <div class="grid gap-6">
+                <div class="">
+                    W środę 4 października w siedzibie Europejskiego Centrum Solidarności w Gdańsku odbyły się
+                    Regionalne obchody Światowego Dnia Turystyki, w którym brali udział studenci III roku Zarządzania
+                    Turystyki i Hotelarstwa. Podczas tego wydarzenia wręczone zostały nagrody i wyróżnienia za zasługi
+                    dla rozwoju turystyki w województwie pomorskim. W grupie nagrodzonych pojawiła się opiekun koła
+                    Naukowego Ekonomiki i Usług Marzena Wanagos.
+                </div>
+                <div class="">
+                    Międzynarodowe Targi Turystyczne TT WARSAW W dniu 23 listopada 2023r. odbył się wyjazd do Warszawy
+                    na Międzynarodowe Targi Turystyczne TT WARSAW w ramach działalności Naukowego koła Ekonomiki i Usług
+                    ,,WannaGo’’. Dla uczestników wyjazdu na terenie targów zostało zorganizowane spotkanie z
+                    pracownikami Polskiej Organizacji Turystycznej.
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
