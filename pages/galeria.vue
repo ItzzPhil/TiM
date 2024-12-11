6eafe8680ebcaab7ac807adbb0319270a7892889
Filip Rad <filip.radziminski@wp.pl>
2024-12-11 23:25:52 +0100

    few minor updates z zęmstą

--- pages/galeria.vue
+++ pages/galeria.vue
@@ -1,11 +1,11 @@
 <template>
     <main class="h-screen w-screen">
-        <div class="pt-40 pb-12 font-extrabold text-center text-9">
+        <div class="pt-40 pb-12 font-bold text-center text-9">
             <div class="">Galeria zdjęć</div>
         </div>
         <div class="grid text-center justify-center gap-12">
             <div class="grid gap-6">
-                <div class="text-6">Wyjazd do Słupska w ramach koła naukowego 15 czerwca 2023r.</div>
+                <div class="text-5">Wyjazd do Słupska w ramach koła naukowego 15 czerwca 2023r.</div>
                 <div class="flex">
                     <div class="w-120" v-for="photo in gallerySlupsk">
                         <Photo :src_dsk="photo.img" />
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <div class="grid gap-6">
-                <div class="text-6">Dni turystyki w Gdańsku</div>
+                <div class="text-5">Dni turystyki w Gdańsku</div>
                 <div class="flex">
                     <div class="w-120 h-90" v-for="photo in galleryGdansk">
                         <Photo :src_dsk="photo.img" />
