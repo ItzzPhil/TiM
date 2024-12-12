14f243f560dddfb21bbe450c28325f593ff35f4d
Filip Rad <filip.radziminski@wp.pl>
2024-12-12 21:56:54 +0100

    added photo

--- components/sections/index/aboutUs.vue
+++ components/sections/index/aboutUs.vue
@@ -43,7 +43,7 @@ export default {
             },
             {
                 name: 'Aleksander Sapko',
-                img: require('assets/img/people/people.png'),
+                img: require('assets/img/people/A_Sapko.jpg'),
                 description: 'Sekretarz',
             },
         ],
