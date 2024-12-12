10fb23e580ccf959a556f91be9d4ac8c1f1a3614
Filip Rad <filip.radziminski@wp.pl>
2024-12-12 16:08:56 +0100

    achievements added

--- pages/osiagniecia.vue
+++ pages/osiagniecia.vue
@@ -1,7 +1,12 @@
 <template>
-    <main class="">
-        <div class="p-6 font-extrabold text-center text-8">
-            <div class="">TEST Osiągnięcia</div>
+    <main class="text-center pb-12">
+        <div class="pt-40 pb-6 font-bold text-9">
+            <div class="">Work In Progress</div>
+            <div class="flex justify-center">
+                <div class="w-185 h-185">
+                    <Photo class="" :src_dsk="require('assets/img/patrick.png')" />
+                </div>
+            </div>
         </div>
     </main>
 </template>
